564a31da5b2944183d76ed7ae53b743ab0f86d85
mpanico20 <m.panico20@studenti.unisa.it>
2025-07-08 16:13:54 +0200

    Added the creation of the second vc and the vp, the check for both

---
+++ xx_create_vc_hotel.js
@@ -0,0 +1,107 @@
+const { Web3 } = require('web3');
+const { EthrDID } = require('ethr-did');
+const { createVerifiableCredentialJwt, verifyCredential } = require('did-jwt-vc');
+const { Resolver } = require('did-resolver');
+const ethrDidResolver = require('ethr-did-resolver');
+const fs = require('fs');
+const path = require('path');
+
+const counterPath = path.join(__dirname, 'counter.json');
+
+async function createDID(address, privateKey, provider, chainID) {
+    const ethrDid = new EthrDID({
+        identifier: address,
+        privateKey,
+        provider: provider,
+        chainNameOrId: chainID,
+    });
+
+    return ethrDid;
+}
+
+function generaID() {
+    const data = JSON.parse(fs.readFileSync(counterPath));
+    const padded = String(data.counter).padStart(4, '0'); // "0001", "0002", ...
+    const id = `http://hotelx.example/credentials/${padded}`;
+    data.counter++;
+    fs.writeFileSync(counterPath, JSON.stringify(data, null, 2));
+    return id;
+}
+
+async function createVCHotel(issuer, subject, checkInDate) {
+    const CheckOut_date = new Date("2025-07-15T10:45:00Z");
+    const release_date = CheckOut_date;
+    const start = new Date(checkInDate.toDateString());
+    const end = new Date(CheckOut_date.toDateString());
+    const num_notti = Math.ceil((end - start) / (1000*60*60*24));
+
+    const id = generaID();
+    // Create the payload for the Verifiable Credential
+    const vcPayload = {
+      sub: subject,
+      // "nbf" indicates the validity starting from (timestamp in seconds)
+      nbf: Math.floor(Date.now() / 1000),
+      // The "vc" field contains the VC document
+      vc: {
+        "@context": ["https://www.w3.org/2018/credentials/v1"],
+        id: id,
+        type: ["VerifiableCredential", "Hotelx"],
+        issuer: issuer.did,
+        credentialSubject: {
+          id: subject,
+          Stay: {
+            CheckIn: checkInDate,
+            CheckOut: CheckOut_date,
+            N_notti: num_notti,
+            Release: release_date
+          }
+        }
+      }
+    };
+
+    const vcJwt = await createVerifiableCredentialJwt(vcPayload, issuer);
+    return vcJwt;
+}
+
+async function main() {
+  try {
+    const providerUrl = 'HTTP://127.0.0.1:7545';
+    const web3 = new Web3(providerUrl);
+
+    // Retrieve hotel account and chainId from Ganache
+    const accounts = await web3.eth.getAccounts();
+    const chainId = await web3.eth.getChainId();
+    const provider = web3.currentProvider;
+
+    //User data
+    const userAccount = accounts[3];
+    const privateKeyUser = "0x139a2d1597daee5e60cd2098e38f179224a364e7c36038025011a54644fd49ac";
+
+    //Hotel data
+    const hotelAccount = accounts[1];
+    const privateKeyHotel = "0x0b039446a2241a02d745abd0de558356aa8a2711631390ccfcf531b01dcde190";
+
+    //Create User DID
+    const userDID = await createDID(userAccount, privateKeyUser, provider, chainId);
+
+    //Create Hotel DID
+    const hotelDID = await createDID(hotelAccount, privateKeyHotel, provider, chainId);
+
+    console.log("User DID is:", userDID.did);
+    console.log("Hotel DID is:", hotelDID.did);
+
+    //VC creation
+    const CheckIn_date = new Date("2025-07-10T15:10:00Z");
+    const vcJwt = await createVCHotel(hotelDID,userDID.did, CheckIn_date);
+
+    //Saving the vcJwt
+    fs.writeFileSync('vc_jwt.txt', vcJwt, 'utf-8');
+
+  } catch(err) {
+    console.log("Error:", err);
+
+  }
+  
+}
+
+main().catch(console.error);
---
+++ xx_create_vp.js
@@ -0,0 +1,160 @@
+const { Web3 } = require('web3');
+const { EthrDID } = require('ethr-did');
+const {
+  createVerifiableCredentialJwt,
+  createVerifiablePresentationJwt,
+  verifyPresentation,
+  verifyCredential
+} = require('did-jwt-vc');
+const { Resolver } = require('did-resolver');
+const ethrDidResolver = require('ethr-did-resolver');
+const fs = require('fs');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const { exit } = require('process');
+
+
+async function createDID(address, privateKey, provider, chainID) {
+    const ethrDid = new EthrDID({
+        identifier: address,
+        privateKey,
+        provider: provider,
+        chainNameOrId: chainID,
+    });
+
+    return ethrDid;
+}
+
+async function createVP(vcJwt_h, vcJwt_b, subject) {
+    const vpPayload = {
+        vp: {
+        "@context": ["https://www.w3.org/2018/credentials/v1"],
+        type: ["VerifiablePresentation"],
+        verifiableCredential: [vcJwt_h, vcJwt_b]
+        }
+    };
+
+    const vpJwt = await createVerifiablePresentationJwt(vpPayload, subject);
+    return vpJwt;
+    
+}
+
+
+async function checkVP(subject, issuer_h, issuer_b, vpJwt, address_r, chainId, providerUrl) {
+    // === CONFIGURATION OF THE RESOLVER ===
+    const registryAddress = address_r; // <-- replace with the DID contract address
+    const resolverConfig = {
+        networks: [{
+            name: chainId,
+            rpcUrl: providerUrl,
+            chainId: chainId,
+            registry: registryAddress
+        }]
+    };
+    const didResolver = new Resolver(ethrDidResolver.getResolver(resolverConfig));
+
+    try {
+        const result = await verifyPresentation(vpJwt, didResolver);
+
+        //Check on the subject
+        if(result.issuer != subject.did){
+            console.log("The subject and the issuer of the vp are not equal!");
+            exit;
+        }
+
+        const decoded = jwt.decode(vpJwt, { complete: true });
+
+        const vpPayload = decoded.payload;
+        const vcs = vpPayload.vp.verifiableCredential;
+
+        //Check for the hotel's vc
+        const verifiedVCh = await verifyCredential(vcs[0], didResolver);
+        const verifiedVCb = await verifyCredential(vcs[1], didResolver);
+        if(verifiedVCh.issuer != issuer_h.did){
+            console.log("The hotel's vc is not signed correctly!");
+            exit;
+        } else if(verifiedVCb.issuer != issuer_b.did){
+            console.log("The booking's vc is non signed correctly!");
+            exit;
+        }
+        
+        const decoded_h = jwt.decode(vcs[0], { complete: true});
+        const decoded_b = jwt.decode(vcs[1], {complete: true});
+        const subjH = decoded_h.payload.vc.credentialSubject.id;
+        const subjB = decoded_b.payload.vc.credentialSubject.id;
+
+        if(result.issuer != subjH || result.issuer != subjB){
+            console.log("The subject is not correct!");
+            exit;
+        }
+
+        //DA IMPLEMENTARE:
+        //1) Controlli per capire che le 2 vc sono collegate, quindi sulle notti
+        //2) Creazione salt + salvataggio hash(id vc hotel + salt) da vedere dove o creare un Map in js e poi salvare
+        //oppure salvare su json
+
+        console.log("The vcs are:", subjB);
+
+    } catch(err){
+        console.log("Error:", err);
+    }
+
+
+    
+}
+
+
+async function main() {
+  try {
+    const providerUrl = 'HTTP://127.0.0.1:7545';
+    const web3 = new Web3(providerUrl);
+
+    // Retrieve hotel account and chainId from Ganache
+    const accounts = await web3.eth.getAccounts();
+    const chainId = await web3.eth.getChainId();
+    const provider = web3.currentProvider;
+
+    //User data
+    const userAccount = accounts[3];
+    const privateKeyUser = "0x139a2d1597daee5e60cd2098e38f179224a364e7c36038025011a54644fd49ac";
+
+    //Hotel data
+    const hotelAccount = accounts[1];
+    const privateKeyHotel = "0x0b039446a2241a02d745abd0de558356aa8a2711631390ccfcf531b01dcde190";
+
+    //Booking data
+    const bookingAccount = accounts[9];
+    const privateKeyBooking = "0x637b8191a4b48aa684cf97f80b43bfb3f0784a8ee156409583fd529edac40383";
+
+    //Create User DID
+    const userDID = await createDID(userAccount, privateKeyUser, provider, chainId);
+
+    //Create Hotel DID
+    const hotelDID = await createDID(hotelAccount, privateKeyHotel, provider, chainId);
+
+    //Create booking DID
+    const bookingDID = await createDID(bookingAccount, privateKeyBooking, provider, chainId);
+
+    console.log("User DID is:", userDID.did);
+    console.log("Hotel DID is:", hotelDID.did);
+    console.log("Booking DID is:", bookingDID.did);
+
+    //Readaing the VCs
+    const vcJwt_h = fs.readFileSync("vc_Jwt.txt", 'utf-8');
+    const vcJwt_b = fs.readFileSync("vc_jwt_booking.txt",'utf-8');
+
+    //VP creation
+    const vpJwt = await createVP(vcJwt_h, vcJwt_b, userDID);
+
+    const address_r = "0x66fe5bE386de94499A00F6C44b60a932D84E4BDe";
+
+    await checkVP(userDID, hotelDID, bookingDID, vpJwt, address_r, chainId, providerUrl);
+
+  } catch(err) {
+    console.log("Error:", err);
+
+  }
+  
+}
+
+main().catch(console.error);
